a57744204d00f499fa5e493012e63a6c6b40032b
Alexarina <alexarinia@gmail.com>
2022-06-03 18:16:16 +0300

    Team list routes

---
+++ app/Http/Controllers/Api/TeamController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TeamCollection;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+    public function getTeamsList() {
+        return new TeamCollection(Team::all());
+    }
+}
