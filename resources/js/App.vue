af29028b17c3932fa9924a092b0bb1a53e7eea9f
Alexarina <alexarinia@gmail.com>
2022-06-03 17:46:50 +0300

    Main page, teams page

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="text-center" style="margin: 20px 0px 20px 0px;">
+            <span class="text-secondary">Insider Champions League</span>
+        </div>
+        <league-teams></league-teams>
+    </div>
+</template>
+
+<script>
+import LeagueTeams from './components/LeagueTeams';
+
+export default {
+    components: {
+        LeagueTeams
+    }
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+window.Vue = require('vue');
+
+import App from './App.vue';
+createApp(App).mount('#app');
